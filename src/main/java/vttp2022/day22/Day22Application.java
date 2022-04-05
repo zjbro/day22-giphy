96e87fb9ef0b96e19c18e58ac5cc8d7b8d4b924a
zjbro <azengjin@gmail.com>
2022-04-05 11:05:52 +0800

    initial commit

---
+++ src/main/java/vttp2022/day22/Day22Application.java
@@ -0,0 +1,13 @@
+package vttp2022.day22;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Day22Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Day22Application.class, args);
+	}
+
+}
---
+++ src/test/java/vttp2022/day22/Day22ApplicationTests.java
@@ -0,0 +1,13 @@
+package vttp2022.day22;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Day22ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
